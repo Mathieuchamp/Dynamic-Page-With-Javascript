4cdf47a4c6bf424a41b28189426231d6679f836a
Mathieu champ <champetier.mathieu@orange.fr>
2024-08-19 09:22:51 +0200

    11eme images + filters

--- FrontEnd/assets/js/userConnection.js
+++ FrontEnd/assets/js/userConnection.js
@@ -1,22 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-   const loginLink = document.getElementById('loginLink');
-   const token = localStorage.getItem('token');
-   const boutonModifier = document.querySelector('.boutonmodifier');
+    const loginLink = document.getElementById('loginLink');
+    const token = localStorage.getItem('token');
+    const boutonModifier = document.querySelector('.boutonmodifier');
+    const filters = document.querySelector('.filters');
+ 
+    if (token) {
+        loginLink.textContent = 'logout';
+        loginLink.style.cursor = 'pointer';
+        loginLink.style.color = 'black';
+        navEdition.style.display = 'flex';
+ 
+        if (boutonModifier) {
+            boutonModifier.style.display = 'inline';
+        }
 
-
-   if (token) {
-       loginLink.textContent = 'logout';
-       loginLink.style.cursor= 'pointer';
-       loginLink.style.color = 'black';
-       navEdition.style.display = 'flex';
-       
-       if (boutonModifier) {
-        boutonModifier.style.display = 'inline';
+        if (filters) {
+            filters.remove();
+        }
+ 
+        loginLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            localStorage.removeItem('token');
+            window.location.href = './index.html';
+        });
     }
-       loginLink.addEventListener('click', function(event) {
-           event.preventDefault();
-           localStorage.removeItem('token');
-           window.location.href = './index.html';
-       });
-   }
-});
+ });
